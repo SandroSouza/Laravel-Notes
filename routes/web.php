00640274ad94fbb50a9ed0464fcac32031a578f6
Sandro Souza <sandro.2005.02.11@gmail.com>
2024-11-16 14:36:54 -0300

    001

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "hello world";
+});
+
+Route::get('/about', function () {
+    echo 'about us';
 });
