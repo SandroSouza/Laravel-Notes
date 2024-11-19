53a5ec1e4eee28a4f2ac4e98e1a74f7af5ca8b38
Sandro Souza <sandro.2005.02.11@gmail.com>
2024-11-18 21:10:30 -0300

    003

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -32,21 +33,40 @@ class AuthController extends Controller
         );
 
         // get user input
-        $username = $request->input('text_username');
+        $email = $request->input('text_username');
         $password = $request->input('text_password');
 
-        // test database connection
-        try{
-            DB::connection()->getPdo();
-            echo 'connection established';
-        } catch (\PDOException $e) {
-            echo "Connection Failed: " . $e->getMessage();
+        //check if user exists
+        $user = User::where('email', $email)->where('deleted_at', NULL)->first();
+
+        if(!$user){
+            return redirect()->back()->withInput()->with('loginError', 'Email ou Senha estão incorretos');
+
         }
 
-        echo "fim";
+        // check if passowrd is correct
+        if(!password_verify($password, $user->password)){
+            return redirect()->back()->withInput()->with('loginError', 'Email ou Senha estão incorretos');
+
+        }
+
+        //update last login
+        $user->last_login = date('Y-m-d H:i:s');
+        $user->save();
+
+        // login user
+        session([
+            'user' => $user->id,
+            'email' => $user->email
+        ]);
+
+        echo 'Login com sucesso';
+
     }
 
     public function logout(){
-        echo "Logout";
+        //logout from the application
+        session()->forget('user');
+        return redirect()->to('/login');
     }
 }
